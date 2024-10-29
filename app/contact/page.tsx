4abe0e8f1c134a0683d482ad05abf8e711d1237d
pro <kutendasamakande21@gmail.com>
2024-10-29 11:18:31 +0200

    added contact form

--- app/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-import Contact from './contact';
-
-export default async function ContactPage() {
-  //const settings = await getSettings();
-  return <Contact />;
-}
-
-// export const revalidate = 60;
