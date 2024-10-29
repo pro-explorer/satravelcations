cc5677c2e19ebe5205c629a38f6368bb4f0d7d0b
pro <kutendasamakande21@gmail.com>
2024-10-29 11:04:57 +0200

    added contact form

--- components/container.tsx
+++ components/container.tsx
@@ -1,16 +1,29 @@
+import { ReactNode } from 'react';
 import { cx } from 'utils/all';
 
-export default function Container(props) {
+interface ContainerProps {
+  children: ReactNode;
+  large?: boolean;
+  alt?: boolean;
+  className?: string;
+}
+
+export default function Container({
+  children,
+  large = false,
+  alt = false,
+  className = ''
+}: ContainerProps) {
   return (
     <div
       className={cx(
         'container mx-auto px-8 xl:px-5',
-        props.large ? 'max-w-screen-xl' : 'max-w-screen-lg',
-        !props.alt && 'py-5 lg:py-8',
-        props.className
+        large ? 'max-w-screen-xl' : 'max-w-screen-lg',
+        !alt && 'py-5 lg:py-8',
+        className
       )}
     >
-      {props.children}
+      {children}
     </div>
   );
 }
