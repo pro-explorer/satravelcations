4133b2fedc8ccd1b660fc83eff55df5b0256d338
pro <kutendasamakande21@gmail.com>
2024-11-25 23:25:52 +0200

    refactor code

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { FaArrowRight } from 'react-icons/fa';
+import { RiArrowRightSLine, RiCompassDiscoverLine } from "react-icons/ri";
 
 const Hero = () => {
   return (
@@ -19,18 +21,9 @@ const Hero = () => {
               New
             </span>{' '}
             <span className="text-sm font-medium">See what's new</span>
-            <svg
-              className="ml-2 h-5 w-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clipRule="evenodd"
-              />
-            </svg>
+
+            <RiArrowRightSLine className="ml-2 h-5 w-5" />
+           
           </a>
           <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl">
             Let's take a tour Cape Town!
@@ -40,39 +33,23 @@ const Hero = () => {
             expertly curated experiences. Unwind in style, from lavish resorts to exclusive escapes!
           </p>
 
+
           <div className="mb-8 flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-x-4 sm:space-y-0 lg:mb-16">
             <a
               href="/about"
               className="inline-flex animate-pulse items-center justify-center rounded-lg bg-primary-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
             >
               Learn more
-              <svg
-                className="-mr-1 ml-2 h-5 w-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
+
+              <FaArrowRight className="-mr-1 ml-2 h-5 w-5" />
+           
             </a>
 
             <Link
               href="/search"
-              className="inline-flex rounded-lg border-2 border-white bg-transparent px-8 py-3 text-lg font-semibold text-white transition duration-300 hover:bg-white hover:text-blue-600"
+              className=" items-center justify-center inline-flex rounded-lg border-2 border-white bg-transparent px-8 py-3 text-lg font-semibold text-white transition duration-300 hover:bg-white hover:text-blue-600"
             >
-              <svg
-                className="-ml-1 mr-2 h-5 w-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <title>WPExplorer</title>
-                <path d="M24 12A12 12 0 1 1 12 0a12.008 12.008 0 0 1 12 12Zm-1.5 0A10.5 10.5 0 1 0 12 22.5 10.516 10.516 0 0 0 22.5 12ZM7.542 5.841l4.074 1.739-1.739 4.073L5.8 9.914l1.742-4.073Zm5.158 7.926 2.185 4.406H14.2l-2.343-4.687-2.295 4.687h-.656l2.4-5.01-1.046-.441.282-.656 3.215 1.364-.281.67Zm-.553-5.451 3.216 1.378-1.378 3.2-3.2-1.364 1.364-3.215Zm3.764 2.011 2.56 1.082-1.1 2.546-2.545-1.083 1.082-2.545Z" />
-              </svg>
+                 <RiCompassDiscoverLine className="mr-2 -ml-1 h-5 w-5" />
               Explore packages
             </Link>
           </div>
