5c0a3963eabaad149d096f4a182edacd1e65089d
pro <kutendasamakande21@gmail.com>
2024-12-06 15:54:36 +0200

    fixed architecture

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>SA Travelcations | Your Gateway to Unforgettable Adventures</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="Discover South Africa and beyond with SA Travelcations. From luxury escapes to adventurous getaways, we create tailor-made travel experiences just for you. Start your dream journey today!"
+      />    
+      <link rel="icon" href="/images/favicon.ico" />
+    </>
+  );
+}
