266bdb824de3002696bee86f425cfa507aa82371
pro <kutendasamakande21@gmail.com>
2025-02-05 04:49:21 +0200

    add google-site-verification

--- app/head.tsx
+++ app/head.tsx
@@ -6,8 +6,9 @@ export default function Head() {
       <meta
         name="description"
         content="Discover South Africa and beyond with SA Travelcations. From luxury escapes to adventurous getaways, we create tailor-made travel experiences just for you. Start your dream journey today!"
-      />    
+      />
       <link rel="icon" href="/images/favicon.ico" />
+      <meta name="google-site-verification" content="DlfQEBUPKxYaNh3fm33h8SJ575zxc5CbQW3wyN01CTM" />
     </>
   );
 }
